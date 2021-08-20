f98bc1d01ff0478ee45831f732381ef108144b05
Saniya Khan <88662781+Saniyakhan786@users.noreply.github.com>
2021-08-20 21:35:56 +0530

    Add files via upload

---
+++ T and Z - Distribution.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from scipy import stats
+import numpy as np
+import pandas as pd
+import math as mt
+
+
+# In[ ]:
+
+
+#T- Distribution
+
+
+# In[ ]:
+
+
+stats.t.ppf( ,df= )
+
+
+# In[ ]:
+
+
+(stats.t.ppf( , )*( /np.sqrt()))+            #formula used is
+#t1-alpha,n-1*Standard deviation/sqrt+sample mean
+
+
+# In[ ]:
+
+
+(-stats.t.ppf( , )*( /np.sqrt()))+   
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#Z- Distribution
+
+
+# In[ ]:
+
+
+stats.norm.ppf(.)
+
+
+# In[ ]:
+
+
+z= stats.norm.ppf(.)
+
+
+# In[ ]:
+
+
+x_bar=
+
+
+# In[ ]:
+
+
+x_bar+z*(  /np.sqrt())
+
+
+# In[ ]:
+
+
+x_bar-z*(  /np.sqrt())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
